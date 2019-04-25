449900ca17bae07f1273da3695d73a02516969ff
Max <40323650+Momo270113@users.noreply.github.com>
2019-04-25 16:35:59 +0300

    Update CreateScheduledTask.ps1

--- CreateScheduledTask.ps1
+++ CreateScheduledTask.ps1
@@ -1,9 +1,3 @@
-﻿#Create a directory to put powershell script 
-mkdir C:\ScheduledTask -Force 
-
-#Copy the file from source to Description 
-Copy-Item -Path "C:\autoupdate\1.ps1" -Destination "C:\ScheduledTask\update.ps1" -Force 
-
 #Create the action 
 $action = New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Argument "powershell.exe -executionpolicy bypass -file C:\ScheduledTask\update.ps1" 
 
@@ -21,7 +15,7 @@ $Params =@{
     "User" = $Username 
     "Password" = $Password 
     "RunLevel" = "Highest" 
-    "Description" = "AutoUpdate Win10 1607 to 1803" 
+    "Description" = "AutoUpdate Win10 XXX to XXX" 
 } 
 
 Register-ScheduledTask @Params
